4516a377643de6b0d559d4de8f707133142b95f9
amarten <alexdmarten@gmail.com>
2021-11-27 15:38:12 -0800

    adding in question bank to be used in logic later on

---
+++ assets/js/questions.js
@@ -0,0 +1,103 @@
+const testQuestions = [
+    {
+        questionTextContent: "What is hoisting?",
+        questionsAnswers: [
+            {
+                answerText: "The act of lifting a design on a webpage to the top.",
+                correct: false,
+            },
+            {
+                answerText: "When declaring a variable or function in JavaScript, the variable/function is compiled at the top of the script.",
+                correct: true,
+            },
+            {
+                answerText: "When a person lifts their pants up and tightens their belt.",
+                correct: false,
+            },
+            {
+                answerText: "When a variable declared inside a function is compiled to the top of the global script.",
+                correct: false,
+            },
+        ],
+
+        questionTextContent: "What is bootstrap?",
+        questionsAnswers: [
+            {
+                answerText: "A third party API designed to make formatting a website's layout in a grid with columns.",
+                correct: true,
+            },
+            {
+                answerText: "A leather strap used back in the 1800s by cowboys to keep their boots from falling off while riding a horse.",
+                correct: false,
+            },
+            {
+                answerText: "A programming language.",
+                correct: false,
+            },
+            {
+                answerText: "A CSS framework that is built into the CSS language.",
+                correct: false,
+            },
+        ],
+
+        questionTextContent: "What is an API?",
+        questionsAnswers: [
+            {
+                answerText: "An artificial programmable intelligence.",
+                correct: false,
+            },
+            {
+                answerText: "A variant programming language of JavaScript.",
+                correct: false,
+            },
+            {
+                answerText: "A set of functions and procedures allowing the creation of applications which access the features/data of an OS, App, or other service.",
+                correct: true,
+            },
+            {
+                answerText: "A unique program that can check your code for errors.",
+                correct: false,
+            },
+        ],
+
+        questionTextContent: "What is a Media Query in CSS?",
+        questionsAnswers: [
+            {
+                answerText: "A plugin that allows multiple media sources to display on your webpage.",
+                correct: false,
+            },
+            {
+                answerText: "A feature in CSS that allows different content rendering based on certain conditions, like screen size.",
+                correct: true,
+            },
+            {
+                answerText: "An inquiry from a media outlet into a recent event.",
+                correct: false,
+            },
+            {
+                answerText: "A third party API that makes content styling in CSS much easier.",
+                correct: false,
+            },
+        ],
+
+        questionTextContent: "Which option below shows the correct utilization of a jQuery?",
+        questionsAnswers: [
+            {
+                answerText: "document.getElementById('demo').innerHTML = 'Hello, World!';",
+                correct: false,
+            },
+            {
+                answerText: "jQuery('demo').innerHTML = 'Hello World!';",
+                correct: false,
+            },
+            {
+                answerText: "$('#demo').html('Hello, World!');",
+                correct: true,
+            },
+            {
+                answerText: "#('.demo').html('Hello, World!')",
+                correct: false,
+            },
+        ],
+    },
+];

03e0fe8a5ed038677b09f26abd458e6f37e49ba0
amarten <alexdmarten@gmail.com>
2021-11-27 16:42:54 -0800

    timer logic setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,35 @@
-var startQuiz = function() {
-    // When the start button is pressed display the first question and answerres
-    // Start the timer in the top right
-}
+var highScores = [];
+var timerEl = $('#timer');
+
+var timerCounter = function() {
+    var timeLeft = 5;
+
+    var timerInterval = setInterval(function() {
+        if (timeLeft >= 0) {
+            var timeString = "Time left: " + timeLeft;
+            $(timerEl).text(timeString);
+            timeLeft--;
+        }
+        else {
+            // Update to have an End game function called here transitioning to the end game page to save name and score to local storage
+            $(timerEl).text("Out of time, game over!")
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+};
+
+timerCounter();
+
+// need to retrieve all high scores from local storage
+
+
+// need to store high score in local storage
+
+
+
+
+// var startQuiz = function() {
+//     // When the start button is pressed display the first question and answerres
+//     // Start the timer in the top right
+// }
